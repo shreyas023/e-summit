3816d06377f40428331623c041d90908bb728696
shreyas023 <shreyas.bailkar023@nmims.edu.in>
2024-02-09 12:43:09 +0530

    e-summit website

---
+++ src/constants/index.js
@@ -0,0 +1,542 @@
+import {
+
+
+ 
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs2,
+  git,
+  python,
+  energy1,
+  ai1,
+  webdev1,
+  security1,
+ 
+  threejswp,
+  reactandthreewp,
+  threejs2,
+  vuejs,
+  firebase,ADMIN,ex_event,ex_events,fin,int_events,int_event,logs1,logs2,mark1,mark2,PHOTO,pr1,pr2,pres1,pres2,rd1,rd2,spons1,spons2,spons3,tech1,tech2,HASSLE,ESUMMIT,COLLABS,NEC
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+    
+  },
+  {
+    id: "work",
+    title: "Ongoing Initiatives",
+    
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    
+  },
+  {
+    id: "Archives",
+    title: "Archives",
+ 
+  },
+  {
+    id: "team",
+    title: "Team",
+   
+  }
+];
+
+const TeamNames = [
+  {
+    title: "Dr. Aparna Rao",
+    subtitle: "Faculty in-charge",
+    icon: "https://media.licdn.com/dms/image/D5603AQGY-5caNseUzg/profile-displayphoto-shrink_100_100/0/1677745809397?e=1712793600&v=beta&t=tYe4RbQd6hlm_baW6sp93ENZLkwk4Z7gNdXND925r-I",
+  },
+  {
+    title: "Sairaj Patil",
+    subtitle: "President",
+    icon: pres1,
+  },
+  {
+    title: "Jahnvi Yadwad",
+    subtitle: "President",
+    icon: pres2,
+  },
+  {
+    title: "Bhoomi Nelwade",
+    subtitle: "Finance Head",
+    icon: fin,
+  },
+  {
+    title: "Tanishq Kaul",
+    subtitle: "Technical Head",
+    icon: tech1,
+  },
+  {
+    title: "Shreyas Bailkar",
+    subtitle: "Technical Head",
+    icon: tech2,
+  },
+  {
+    title: "Kunal Shenoy",
+    subtitle: "Admin Head",
+    icon: ADMIN,
+  },
+  {
+    title: "Astha Kathar",
+    subtitle: "PR Head",
+    icon: pr1,
+  },
+  {
+    title: "Smit Patil",
+    subtitle: "PR Head",
+    icon: pr2,
+  },
+  {
+    title: "Arsh Mathur",
+    subtitle: "Event Interal Head",
+    icon: int_events,
+  },
+  {
+    title: "Antariksh Rajkonwar",
+    subtitle: "Event Interal Head",
+    icon: int_event,
+  },
+  {
+    title: "Navneeth Krishnan",
+    subtitle: "Marketing Head",
+    icon: mark1,
+  },
+  {
+    title: "Aarya Mishra",
+    subtitle: "Marketing Head",
+    icon: mark2,
+  },
+  {
+    title: "Aum Pradhan",
+    subtitle: "R&D Head",
+    icon: rd1,
+  },
+  {
+    title: "Abhinav Kidav",
+    subtitle: "R&D Head",
+    icon: rd2,
+  },
+  {
+    title: "Aaditya Singh",
+    subtitle: "Logistics Head",
+    icon: logs2,
+  },
+  {
+    title: "Lakshmeesh Mankame",
+    subtitle: "Logistics Head",
+    icon: logs1,
+  },
+  {
+    title: "Amarti Dhamele",
+    subtitle: "Events External Head",
+    icon: ex_event,
+  },
+  {
+    title: "Shreyans Rath",
+    subtitle: "Events External Head",
+    icon: ex_events,
+  },
+  {
+    title: "Shishir Walvekar",
+    subtitle: "Sponsorship Head",
+    icon: spons3,
+  },
+  {
+    title: "Jaival Desai",
+    subtitle: "Sponsorship Head",
+    icon: spons2,
+  },
+  {
+    title: "Shravani Devke",
+    subtitle: "Dynamic Media Head",
+    icon: PHOTO,
+  },
+
+];
+const services = [
+  {
+    title: "Halsepreneurs",
+    
+    icon: HASSLE,
+  },
+  {
+    title: " NEC",
+    icon: NEC,
+  },
+  {
+    title: "E-summit",
+    icon: ESUMMIT,
+    
+  },
+  {
+    title: "Collabs",
+    icon: COLLABS,
+  },
+];
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "nodejs2",
+    icon: nodejs2,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "threejs2",
+    icon: threejs2,
+  },
+  {
+    name: "firebase",
+    icon: firebase,
+  },
+  {
+    name: "vuejs",
+    icon: vuejs,
+  },
+];
+
+const experiences = [
+  {
+    title: "InnovateX",
+    link: "https://www.instagram.com/reel/C2RIwT5x0nY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "12th Jan 2024",
+    points: [
+      "InnovateX event by E-Cell, GDSC, and Institutions Innovation Council at NMIMS Navi Mumbai.",
+      "Aimed to explore entrepreneurship in India and provide practical guidance.",
+      "Featured Faiz Malkani, co-founder of Rippl, to inspire and equip students.",
+      "Successfully bridged theoretical knowledge and practical challenges.",
+      "Malkani's engaging address and interactive Q&A left a lasting impact.",
+      "Facilitated networking opportunities and mentor connections.",
+      "Positive feedback and enriching experience showcased event's success.",
+      "Sparked inspiration and motivation for entrepreneurial endeavours.",
+      "Collaboration highlighted effectiveness of joint efforts in fostering innovation.",
+      "Expected impact on continued growth of entrepreneurial initiatives within NMIMS community.",
+    ],
+  },
+  {
+    title: "Shark Tank",
+    link: "https://www.instagram.com/reel/C0_e6aJRKaw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "20th Dec 2023",
+    points: [
+      "Shark Tank event hosted by E-Cell of STME, conducted offline and online for participant convenience.",
+      "Three rounds: quiz, pitching, advertising.",
+      "Top teams select product for pitch, others choose from remaining options."
+,      "Pitching judged on presentation, participation, etc.",
+      "Advertising reel posted for voting.",
+      "Single round with teams presenting ideas and solutions.",
+      "Judged by faculty member Dr. Toral Shah.",
+      "Overall, a 2-day event: offline on first day, online on second.",
+      "Great experience working with fellow students.",
+    ],
+  },
+  {
+    title: "Indo-German Collaboration Session",
+    link: "https://www.instagram.com/reel/C0zSHpuxN_E/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "8th Dec 2023",
+    points: [
+      "E-Cell of STME, Navi Mumbai organized interaction with Technical University, Braunschweig (TU BS), Germany.",
+      "Focus: Collaboration for Indo-German Centre for Entrepreneurship.",
+      "Faculty discussed NMIMS student opportunities in Summer Camp (Offline) and Summer School (Online), research, and exchange activities.",
+      "Students engaged on cross-border entrepreneurship vision and objectives.",
+      "Closing: Student entrepreneurs presented startup projects, receiving positive feedback.",
+      "Well-received event by faculty and students, anticipating future collaboration.",
+      "Objective: Explore collaboration for NMIMS Navi Mumbai and TU BS within Indo-German Centre.",
+      "Outcome: Base established for robust cross-border entrepreneurship relationship, fostering innovation and startup opportunities.",
+    ],
+  },
+  {
+    title: "The Hass-le preneurs",
+    link: "https://www.instagram.com/reel/CxfP-T9Lac5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "Sept 2023",
+    points: [
+      "'Haslepreneurs': Dynamic Instagram talk show addressing contemporary issues in scams, policies, and entrepreneurial challenges.",
+      "Weekly episodes feature guest speakers offering fresh perspectives and expertise.",
+      "Primary focus on unraveling scam intricacies and analyzing current policies affecting entrepreneurs.",
+      "Aims to empower audience with knowledge and awareness in online entrepreneurial space.",
+      "Lively and interactive format encourages in-depth discussions, valuable insights, and practical advice.",
+      "Provides platform for navigating evolving online business landscape and avoiding pitfalls.",
+    ],
+  },
+];
+const Archives = [
+  {
+    title: "InnovateX",
+    link: "https://www.instagram.com/reel/C2RIwT5x0nY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "12th Jan 2024",
+    points: [
+      "InnovateX event by E-Cell, GDSC, and Institutions Innovation Council at NMIMS Navi Mumbai.",
+      "Aimed to explore entrepreneurship in India and provide practical guidance.",
+      "Featured Faiz Malkani, co-founder of Rippl, to inspire and equip students.",
+      "Successfully bridged theoretical knowledge and practical challenges.",
+      "Malkani's engaging address and interactive Q&A left a lasting impact.",
+      "Facilitated networking opportunities and mentor connections.",
+      "Positive feedback and enriching experience showcased event's success.",
+      "Sparked inspiration and motivation for entrepreneurial endeavours.",
+      "Collaboration highlighted effectiveness of joint efforts in fostering innovation.",
+      "Expected impact on continued growth of entrepreneurial initiatives within NMIMS community.",
+    ],
+  },
+  {
+    title: "Shark Tank",
+    link: "https://www.instagram.com/reel/C0_e6aJRKaw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "20th Dec 2023",
+    points: [
+      "Shark Tank event hosted by E-Cell of STME, conducted offline and online for participant convenience.",
+      "Three rounds: quiz, pitching, advertising.",
+      "Top teams select product for pitch, others choose from remaining options."
+,      "Pitching judged on presentation, participation, etc.",
+      "Advertising reel posted for voting.",
+      "Single round with teams presenting ideas and solutions.",
+      "Judged by faculty member Dr. Toral Shah.",
+      "Overall, a 2-day event: offline on first day, online on second.",
+      "Great experience working with fellow students.",
+    ],
+  },
+  {
+    title: "Panel Discussion on Entrepreneurship Ecosystem in HEIs",
+    link: "https://www.linkedin.com/posts/e-cell-nmims-navi-mumbaii_entrepreneurship-leadership-innovation-activity-7143608891858206721-MkLA/?utm_source=share&utm_medium=member_android",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "15th Dec 2023",
+    points: [
+      "Panel Discussion on Entrepreneurship Ecosystem by NMIMS Navi Mumbai and E-Cell, STME.",
+      "Led by Dr. Aparna Rao and Mr. Nilesh Lele.",
+      "Theme: Entrepreneurship in Institutions: Students and Faculty as Changemakers.",
+      "Mr. Lele discussed fostering innovation and shared experiences.",
+      "Dr. Rao highlighted student entrepreneurs' stories and emphasized an entrepreneurial mindset.",
+      "Urged for entrepreneurship education and supportive ecosystems.",
+      "Referenced IIT Bombay as a role model.",
+      "Aimed to inspire active engagement in developing the entrepreneurial ecosystem at NMIMS Navi Mumbai.",
+      "Well received by faculty and students.",
+    ],
+  },
+  {
+    title: "Indo-German Collaboration Session",
+    link: "https://www.instagram.com/reel/C0zSHpuxN_E/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon:COLLABS,
+    iconBg: "#383E56",
+    date: "8th Dec 2023",
+    points: [
+      "E-Cell of STME, Navi Mumbai organized interaction with Technical University, Braunschweig (TU BS), Germany.",
+      "Focus: Collaboration for Indo-German Centre for Entrepreneurship.",
+      "Faculty discussed NMIMS student opportunities in Summer Camp (Offline) and Summer School (Online), research, and exchange activities.",
+      "Students engaged on cross-border entrepreneurship vision and objectives.",
+      "Closing: Student entrepreneurs presented startup projects, receiving positive feedback.",
+      "Well-received event by faculty and students, anticipating future collaboration.",
+      "Objective: Explore collaboration for NMIMS Navi Mumbai and TU BS within Indo-German Centre.",
+      "Outcome: Base established for robust cross-border entrepreneurship relationship, fostering innovation and startup opportunities.",
+    ],
+  },
+  {
+    title: "Tradiosity",
+    link: "https://www.linkedin.com/posts/e-cell-nmims-navi-mumbaii_empowering-future-investors-the-tradiosity-activity-7133394072429314050-Z3TM?utm_source=share&utm_medium=member_desktop",
+    icon:COLLABS,
+    iconBg: "#383E56",
+    date: "12th Oct 2023",
+    points: [
+      "Tradiosity Department of E-Cell, SOC is dedicated to enhancing the financial acumen of young investors through insightful weekly trading sessions.",
+      "Four successful sessions have been conducted, eliciting enthusiastic feedback from participants.",
+      "Sessions cover stock trading fundamentals comprehensively, including compounding, basics of stock markets, Nifty 50 & Sensex insights, trading terminologies, diverse trading styles, and creating personalized stock watchlists.",
+      "The goal is to provide students with practical knowledge and essential skills to confidently navigate financial markets.",
+      "Positive responses motivate the team to continue fueling the curiosity of budding investors, encouraging ongoing exploration of the financial landscape.",
+    ],
+  },
+  {
+    title: "The Hass-le preneurs",
+    link: "https://www.instagram.com/reel/CxfP-T9Lac5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    icon: HASSLE,
+    iconBg: "#383E56",
+    date: "Sept 2023",
+    points: [
+      "'Haslepreneurs': Dynamic Instagram talk show addressing contemporary issues in scams, policies, and entrepreneurial challenges.",
+      "Weekly episodes feature guest speakers offering fresh perspectives and expertise.",
+      "Primary focus on unraveling scam intricacies and analyzing current policies affecting entrepreneurs.",
+      "Aims to empower audience with knowledge and awareness in online entrepreneurial space.",
+      "Lively and interactive format encourages in-depth discussions, valuable insights, and practical advice.",
+      "Provides platform for navigating evolving online business landscape and avoiding pitfalls.",
+    ],
+  },
+  {
+    title: "BOOTCAMP’23",
+    link: "https://www.linkedin.com/in/e-cell-nmims-navi-mumbaii/recent-activity/all/",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "15th March-16th March 2023",
+    points: [
+      "Certification course affiliated with IIC to promote entrepreneurial mindset among students.",
+      "This Event was Sponsored by SPRK Technologies and Blackstone.",
+      "Guest speakers invited: Saurabh Jain - Co-founder Stable Money.",
+      "Sagar Chandani – Angel Investor, Startup coach",
+      "Kamal Kumawat - Co-founder at Edgistify",
+      "Sanchit Jain - AWS Ambassador",
+      "Anuj Sharma – Founder of ALSISAR Impact ",
+      "Hardik Prajapati – Director HPC Engineering co.",
+    ],
+  },
+  {
+    title: "FabLab at RIDDL",
+    link: "https://www.linkedin.com/in/e-cell-nmims-navi-mumbaii/recent-activity/all/",
+    company_name: "KJ Somaiya Campus",
+    icon: ai1,
+    iconBg: "#383E56",
+    date: "  25th February 2023",
+    points: [
+      "Objective: To gain exposure to the latest technologies and explore the possibilities of digital fabrication",
+      " A chance for us to meet like-minded people and to get inspired by the innovative projects being developed at the FabLab.",
+    ],
+  },
+  {
+    title: "National Entrepreneurship Challenge (NEC)",
+    link: "https://www.linkedin.com/in/e-cell-nmims-navi-mumbaii/recent-activity/all/",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "Jan 2023",
+    points: [
+      "9 months long event conducted by E-cell of IIT Bombay.",  
+      "NMIMS Navi Mumbai ranked 46th in this nationwide event",
+    ],
+  },
+  {
+    title: "Fablab at Pune",
+    link: "https://www.linkedin.com/in/e-cell-nmims-navi-mumbaii/recent-activity/all/",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "22th Dec 2023",
+    points: [
+      "To create awareness in students about start up incubation and start up ecosystem.",
+       "Expose students to case studies of social innovation and converting them into startups.",
+    ],
+  },
+  {
+    title: "Venturex",
+    link: "https://www.linkedin.com/in/e-cell-nmims-navi-mumbaii/recent-activity/all/",
+    icon: COLLABS,
+    iconBg: "#383E56",
+    date: "30th Aug 2022",
+    points: [
+      "Guest session by Entrepreneurs",
+      "Shaunak Tomar – Founder, HOD – Animal Rights and Advocacy Department Citizens for Animal Protection (CAP) Foundation",
+      " Arjun Deshpande – Founder of Generic Aadhar",
+    ],
+  },
+];
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "ThreeJS",
+    description:
+      "Employing the versatility of JavaScript in conjunction with the robust capabilities of Three.js, my project demonstrates a sophisticated implementation of interactive 3D graphics.",
+    tags: [
+      {
+        name: "javascript",
+        color: "",
+      },
+      {
+        name: "threejs",
+        color: "green-text-gradient",
+      },
+    ],
+    image: threejswp,
+    source_code_link: "https://github.com/gvue003/threejs_portfolio",
+  },
+  {
+    name: "Web Developer Portfolio",
+    description:
+      "My project seamlessly integrates the dynamic user interface capabilities of React.js with the immersive 3D rendering functionalities of Three.js to create a captivating and interactive web experience.",
+    tags: [
+      {
+        name: "javascript",
+        color: "",
+      },
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "threejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: reactandthreewp,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects ,TeamNames,Archives};
